cbe670f63c146f44aab0ff889f04fe9dd7283ddc
Dharmil Shah <dharmilshah005@gmail.com>
2023-06-05 16:11:38 +0530

    Add files via upload

---
+++ Practical 6.3/6.3(8)update.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Table Data</title>
+<style>
+table{
+border-collapse: collapse;
+}
+td,th{
+padding: 5px 15px;
+}
+</style>
+</head>
+<body>
+<?php
+$con = new mysqli("localhost","root","","studentdb");
+if(mysqli_connect_error())
+{
+die("connection fail <br>" . mysqli_connect_error());
+}
+else{
+echo("<small>Connection successful</small> <br>");
+}
+echo "<h1>Updated recored in Student Table </h1>"; echo " <table border=1>
+<tr>
+<th>Enrollment</th>
+<th>Name</th>
+<th>Age</th>
+<th>Marks</th>
+</tr>";
+$sql = "UPDATE `student` SET `marks`=0 WHERE `age` = 30";
+$res = $con->query($sql); 
+if($res)
+{
+$sql = "SELECT * FROM student;";
+$res = $con->query($sql);
+while($row = $res->fetch_array())
+{
+echo "<tr> <td>" . $row['enroll_no'] . "</td> <td>" . $row['name'] . "</td> <td>" .
+$row['age'] . "</td> <td>" . $row['marks'] . "</td> </tr><br>";
+}
+}
+else{
+echo $con->error;
+}
+echo "</table>";
+$con->close();
+?>
+</body>
+</html>
